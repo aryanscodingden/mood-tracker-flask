39358f4d04d2f226bd8a3fb2e50364ecaafec173
Aryan Rao <aryanscode@gmail.com>
2026-02-21 16:49:59 +0530

    new thingys hmmm

--- app.py
+++ app.py
@@ -11,13 +11,35 @@ with get_db() as db:
         CREATE TABLE IF NOT EXISTS moods (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         mood TEXT
+        date TEXT
         )
     """)
 
 @app.route("/")
 def home():
     db = get_db()
-    return render_template("home.html")
+    moods = db.excute("SELECT mood FROM moods ORDER BY id DESC LIMIT 5").fetchall()
+
+    hour = datetime.now().hour
+    if hour < 12:
+        greeting = "Good morning"
+    elif hour < 18:
+        greeting = "Good afternoon"
+    else:
+        greeting = "Good evening"
+
+    return render_template("home.html", moods=moods, greeting=greeting)
+
+@app.route("/add", method = ["GET", "POST"])
+def add():
+    if request.method == "POST":
+        mood = request.form["mood"]
+        date = datetime.now().strftime("%Y-%m-%d")
+
+        db = get_db()
+        db.excute("INSERT INTO moods (mood, date) VALUES (?,?)", (mood, date))
+        db.commit()
+        return redirect("/")
 
 @app.route("/add/<mood>")
 def add(mood):
