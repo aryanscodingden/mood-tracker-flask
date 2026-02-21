9abd0ed2ba638bb9c7ff316113612d51c800abdd
Aryan Rao <aryanscode@gmail.com>
2026-02-21 21:05:10 +0530

    new pages

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ with get_db() as db:
     db.execute("""
         CREATE TABLE IF NOT EXISTS moods (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        mood TEXT
+        mood TEXT,
         date TEXT
         )
     """)
@@ -18,7 +19,7 @@ with get_db() as db:
 @app.route("/")
 def home():
     db = get_db()
-    moods = db.excute("SELECT mood FROM moods ORDER BY id DESC LIMIT 5").fetchall()
+    moods = db.execute("SELECT mood FROM moods ORDER BY id DESC LIMIT 5").fetchall()
 
     hour = datetime.now().hour
     if hour < 12:
@@ -30,23 +31,19 @@ def home():
 
     return render_template("home.html", moods=moods, greeting=greeting)
 
-@app.route("/add", method = ["GET", "POST"])
+@app.route("/add", methods = ["GET", "POST"])
 def add():
     if request.method == "POST":
         mood = request.form["mood"]
         date = datetime.now().strftime("%Y-%m-%d")
 
         db = get_db()
-        db.excute("INSERT INTO moods (mood, date) VALUES (?,?)", (mood, date))
+        db.execute("INSERT INTO moods (mood, date) VALUES (?,?)", (mood, date))
         db.commit()
         return redirect("/")
 
-@app.route("/add/<mood>")
-def add(mood):
-    db = get_db()
-    db.execute("INSERT INTO moods (mood) VALUES (?)", (mood,))
-    db.commit()
     return render_template("add.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
