0474537f126c9e09ba71718973bc8a477ab34473
Aryan Rao <aryanscode@gmail.com>
2026-02-21 16:40:12 +0530

    got buttons & db yummy

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template
+import sqlite3
+
 app = Flask(__name__)
+
+def get_db():
+    return sqlite3.connect("moods.db")
+
+with get_db() as db:
+    db.execute("""
+        CREATE TABLE IF NOT EXISTS moods (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        mood TEXT
+        )
+    """)
 
 @app.route("/")
 def home():
+    db = get_db()
     return render_template("home.html")
 
-@app.route("/add")
-def add():
+@app.route("/add/<mood>")
+def add(mood):
+    db = get_db()
+    db.execute("INSERT INTO moods (mood) VALUES (?)", (mood,))
+    db.commit()
     return render_template("add.html")
 
 if __name__ == "__main__":
